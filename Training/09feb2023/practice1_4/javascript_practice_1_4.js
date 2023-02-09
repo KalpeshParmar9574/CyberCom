69408606153f74e50467940d2f8cedb3ebea5254
kalpesh <kalpeshparmar9574@gmail.com>
2023-02-09 17:25:44 +0530

    tests code is updated

--- Training/09feb2023/practice1_4/javascript_practice_1_4.js
+++ Training/09feb2023/practice1_4/javascript_practice_1_4.js
@@ -1,35 +1,49 @@
-const nums = [6,5,-1,6,-4,5]
-let targetValue=5
 
-function case02(nums, target)
-{
-    nums.sort();
-    var result = nums[0] + nums[1] + nums[2];
-    let i = 0;
-    while (i < nums.length - 1) {
-        var nxt = i + 1;
-        var end = nums.length - 1;
-        while (nxt < end) {
-            var sum = nums[i] + nums[nxt] + nums[end];
-            if (target === sum) {
-                return sum;
-            }
-            if(Math.abs(sum-target) < Math.abs(result - target)){
-                result = sum;
-            }
-            if(sum > target)
-                end -= 1;
-            else
-                nxt += 1;
-        }
-        i++;
-    }
-    return result;
-    }
+//  case01
+// A valid IP address consists of exactly four integers separated by single dots. Each integer is
+// between 0 and 255 (inclusive) and cannot have leading zeros.
+// For example, "0.1.2.201" and "192.168.1.1" are valid IP addresses, but "0.011.255.245",
+// "192.168.1.312" and "192.168@1.1" are invalid IP addresses.
+// Given a string s containing only digits, return all possible valid IP addresses that can be formed
+// by inserting dots into s. You are not allowed to reorder or remove any digits in s. You may
+// return the valid IP addresses in any order.
+
+
+
+
+
+// const nums = [6,5,-1,6,-4,5]
+// let targetValue=5
+
+// function case02(nums, target)
+// {
+//     nums.sort();
+//     var result = nums[0] + nums[1] + nums[2];
+//     let i = 0;
+//     while (i < nums.length - 1) {
+//         var nxt = i + 1;
+//         var end = nums.length - 1;
+//         while (nxt < end) {
+//             var sum = nums[i] + nums[nxt] + nums[end];
+//             if (target === sum) {
+//                 return sum;
+//             }
+//             if(Math.abs(sum-target) < Math.abs(result - target)){
+//                 result = sum;
+//             }
+//             if(sum > target)
+//                 end -= 1;
+//             else
+//                 nxt += 1;
+//         }
+//         i++;
+//     }
+//     return result;
+//     }
 
  
 
-console.log(case02(nums,targetValue))
+// console.log(case02(nums,targetValue))
 
 
 // case3
