2f94faac53e77ed48707957a711a09447e13cd73
kalpesh <kalpeshparmar9574@gmail.com>
2023-02-13 18:39:12 +0530

    callbacks and promises task is done and 2 from  practice set 3 is done with the all three types

---
+++ Training/11feb2023/javascript_practice_3_1.js
@@ -0,0 +1,118 @@
+//***/ case01***//
+
+// Create a web application that contains a form with following fields.
+// First Name
+// Last Name
+// Email
+// Mobile Number
+// Keep all the required validation on the application.
+// Once user click on the submit, use function name as "saveDetails", it
+// should be arrow function and return promise with "resolve" state if
+// all the validations are correct. return "reject" if any of the
+// validation is not correct and display proper error message.
+
+/*** Code  */
+
+// case02 //
+
+
+// Create a web application that contains three async functions,
+// one that contains setTimeout with 5s with return statement as "output
+// from one"
+// second that contains setTimeout with 10s with return statement as
+// "output from second"
+// third that contains setTimeout with 15s with return statement as
+// "output from third"
+// output should be display as following,
+// output from one
+// output from two
+// output from third
+
+//**01 */ code is done by promise
+
+// function one() {
+    
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("function one it is take 5sec for exe ");
+//             return resolve;
+//         },10000)
+//     })
+// }
+// function two() {
+    
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("function two it is take 10sec for exe ");
+//             return resolve;
+//         }, 15000);
+//     });
+// }
+
+// function three() {
+//     console.log("function three it is take 15sec for exe ");
+    
+// }
+// // initial function which is call
+// function callBack() {
+//     setTimeout(() => {
+//         one()
+//     }, 5000);
+    
+// }
+// // calling initial function
+// callBack();
+// // calling first function
+
+// // calling thirdfunction
+// two().then(() => {
+//     three()
+// })
+
+//**02 */ code is done by callbacks
+// function init(one) {
+//     setTimeout((one, two, three) => {
+//         one(two,three);
+//     }5000,);
+// }
+
+// function one(two ) {
+//     console.log(" this code is run after 5 sec ")
+//     setTimeout((three) => {
+//         two(three)
+//     })
+// }
+// function two(three) {
+//     console.log("this function is call after 10 sec")
+//     setTimeout(() => {
+//         three();
+//     },1500)
+// }
+// function three() {
+//     console.log("this function is call after 15 sec")
+// }
+
+//**03*/code is completed with async and awaitc
+async function  display  (){
+    try {
+        await time(5000)
+        function one() {
+            console.log("function one is runn now")
+        }
+        await time(10000)
+         function two() {
+             console.log("function two is runn now")
+          }
+        await time(15000)
+          function three() {
+              console.log("function three is runn now")
+          }
+    } catch {
+        
+    }
+    
+}
+
+
+
+display();
---
+++ Training/11feb2023/js_callback.js
@@ -0,0 +1,70 @@
+// case0 example from the ppt 
+
+
+// function _addTeaLeave() {
+//     console.log("tea leaves are added " + "this function is running after 2sec");
+// }
+// function _addMilk() {
+//     console.log("milk is added..." + "this functon is running after 5 sec");
+// }
+// function _boilWater(addMilk, addTeaLeave) {
+//     console.log("water is boiling now..");
+//     setTimeout(() => {
+//         setTimeout(() => {
+//             addMilk();
+//         }, 3000);
+//         addTeaLeave();
+//     }, 2000)
+
+
+// }
+
+// _boilWater(_addMilk,_addTeaLeave);
+//*** Done  */
+
+
+// Case 1: 
+// Create a web application that contains three callbacks' functions.
+// One function that contains delay of 10s, once this is executed, second 
+// function will start executing (with 10s delay).
+// Once second function will be executed, third function will start 
+// executing (with 8s delay).
+// Each function will have console statement stated as "X function is 
+// start executing
+
+function _one(){
+    console.log("function one is strat execution at 8sec ");
+
+}
+function _two(){
+    console.log("function two is start after  execution after 10sec  ");
+
+}
+function _three(){
+    console.log("function three is start  execution after 10sec");
+
+}
+debugger
+function callBack ( one , two,three){
+    // setTimeout(()=>{
+    //         one()
+    // },8000);
+
+    // setTimeout(()=>{
+    //     two();
+    // },10000)
+    // setTimeout(()=>{
+    //     three()
+    // },10000)
+
+    setTimeout(()=>{
+        three();
+        setTimeout(()=>{
+            two()
+            setTimeout(()=>{
+                one();
+            })
+        },10000)
+    },8000)
+}
+callBack(_one, _two,_three)
