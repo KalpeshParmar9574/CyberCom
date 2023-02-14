2fa17832b95ca606b86a72cd90635128820cb33d
kalpesh <kalpeshparmar9574@gmail.com>
2023-02-14 13:42:28 +0530

    practice 08 form validation code is updated

--- Training/03feb2023/practice08_html/p08.js
+++ Training/03feb2023/practice08_html/p08.js
@@ -1,45 +1,107 @@
 // form validation
+function validation() {
+    var submit = true;
+        var submit = true;
+        // name check 
+          var fname = document.forms['myform']["fname"].value;
+          var lname = document.forms['myform']["fname"].value;
+      
+          if (fname == "" || lname== "") {
+              _erroMsg("Name is to short it is not valid","nameErr")
+              submit = false;
+              return submit
+          }
+          // email check 
+          var email = document.forms['myform']["email"].value;
+          var reEmail = document.forms['myform']["re_email"].value;
+          const regex = /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
+          if (regex.test(email)) {
+              _erroMsg("Email is  not valid");
+              submit = false;
+              return submit;
+          }
+          if (email !== reEmail) {
+              _erroMsg(" Email and Re enter email is not match ","emailErr");
+              submit = false
+              return submit;
+          }
+      
+          // password check
+      
+          var pass = document.forms['myform']["pass"].value;
+          var re_pass = document.forms['myform']["re_pass"].value;
+          if (pass !== re_pass) {
+              _erroMsg("password is not match with re_type password","passErr")
+              submit = flase;
+              return submit;
+          }
+          // note is future mobile check is added 
+          return submit;
 
-const form = document.getElementById('form');
-form.addEventListener('submit',(e)=> {
-    e.defaultPrevented();
     
-})
+
+}
+
+function _erroMsg(errmsg,errid) {
+    // alert(errmsg);
+    var err = document.getElementById(errid);
+    err.textContent = errmsg;
+   
+
+}
 //  function for the data store in local storage
 function storeData() {
     // for personal details
-    var fn = document.getElementById('firstName').value;
-    localStorage.setItem("firstname", fn);
-    var ln = document.getElementById('lastName').value;
-    localStorage.setItem("firstname", ln);
+    var fn = document.getElementById('fname').value;
+    
+    var ln = document.getElementById('lname').value;
+  
     var dob = document.getElementById('dob').value;
-    localStorage.setItem("firstname", dob);
+   
     var gender = document.getElementById('gender').value;
-    localStorage.setItem("firstname", gender);
+
     // for account info
     var em = document.getElementById('email').value;
-    localStorage.setItem("email", em);
-    var pss = document.getElementById('pass').value;
-    localStorage.setItem("pass", pss);
+   
+    var pass = document.getElementById('pass').value;
+  
     var sec_q = document.getElementById('security_question').value;
-    localStorage.setItem("seurityQuestion", sec_q);
+    
     var sec_a = document.getElementById('security_answer').value;
-    localStorage.setItem("security", sec_a);
+    
     // for contact info 
     var add = document.getElementById('addr').value;
-    localStorage.setItem("addr", add);
+   
     var city = document.getElementById('city').value;
-    localStorage.setItem("city", city);
+   
     var state = document.getElementById('state').value;
-    localStorage.setItem("state", state);
+   
     var zcode = document.getElementById('zipcode').value;
-    localStorage.setItem("zipcode", zcode);
+    
     var phn = document.getElementById('phn').value;
-    localStorage.setItem("phn", phn);
+    
     var phn = document.getElementById('phn').value;
-    localStorage.setItem("phn", phn);
+ 
     var tphn = document.getElementById('type_phn').value;
-    localStorage.setItem("type_phn", tphn);
+    
+    var user = {
+        fname: fn,
+        lname: ln,
+        dob: dob,
+        gender: gender,
+        email: em,
+        pass: pass,
+        sec_Q: sec_q,
+        sec_A: sec_a,
+        add: add,
+        city: city,
+        state: state,
+        zipcode: zcode,
+        phNo: phn,
+        
+        
+    }
+    localStorage.setItem(user);
     
 
     
